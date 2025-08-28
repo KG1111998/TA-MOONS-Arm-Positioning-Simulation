e6b88537762710e7387af9df84ce1ba03768ad73
KARTIK GHANSHYAM GOKHE <gokhekartik@gmail.com>
2025-08-29 03:45:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ st.title(" TA-MOONS Robotic Arm Positioning Simulator")
 st.write(""" This project focuses on designing and simulating a collision-free arm positioning mechanism for the TA-MOONS front optics using methodologies. The system allows precise allocation of targets within a circular field of view based on celestial coordinates. 
 The ongoing plan consists of 4 modules where:
 
-* Module 1: Target fetching & visualization (Aladin + Catalog + CSV)
-* Module 2: YSO Grouping and Collision-Free Subset Creation
+* Module 1: Target fetching from Vizier Catalog
+* Module 2: YSO Grouping and Collision-Free Clusters Creation
 * Module 3: Sky Image Retrieval & FITS-based Target Plotting
 * Module 4: Robotic Arm Positioning Simulation for TA-MOONS """)
 
