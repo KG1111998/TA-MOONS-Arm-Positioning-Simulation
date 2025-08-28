34190b900be6ca4db5fe204e270c6c02f9e12ed5
KARTIK GHANSHYAM GOKHE <gokhekartik@gmail.com>
2025-08-29 03:38:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,14 +18,18 @@ import io
 # --- App Configuration ---
 st.set_page_config(layout="wide", page_title="TA-MOONS Arm Simulator")
 st.title(" TA-MOONS Robotic Arm Positioning Simulator")
-st.write(" This project focuses on designing and simulating a collision-free arm positioning mechanism for the TA-MOONS front optics using methodologies inspired by the Kband Multi-object Spectrograph (KMOS) of the Very Large Telescope (VLT) and Multi-object Spectroscopic Mode (MOS) of the High-Resolution Optical Spectrograph (HROS) of the Thirty Meter Telescope (TMT). The system allows precise allocation of targets within a circular field of view based on celestial coordinates. A robust collision avoidance algorithm ensures safe operation, while Monte Carlo simulations validate the system’s reliability. This work demonstrates the integration of celestial mechanics, optical physics, and computational engineering to achieve a scalable solution for next-generation ground based survey telescopic instruments.
+st.write(""" This project focuses on designing and simulating a collision-free arm positioning mechanism for the TA-MOONS front optics using methodologies 
+inspired by the Kband Multi-object Spectrograph (KMOS) of the Very Large Telescope (VLT) and Multi-object Spectroscopic Mode (MOS) of the High-Resolution 
+Optical Spectrograph (HROS) of the Thirty Meter Telescope (TMT). The system allows precise allocation of targets within a circular field of view based on 
+celestial coordinates. A robust collision avoidance algorithm ensures safe operation, while Monte Carlo simulations validate the system’s reliability. 
+This work demonstrates the integration of celestial mechanics, optical physics, and computational engineering to achieve a scalable solution for next-generation ground based survey telescopic instruments.
 
 The ongoing plan consists of 4 modules where:
 
 Module 1: Target fetching & visualization (Aladin + Catalog + CSV)
 Module 2: YSO Grouping and Collision-Free Subset Creation
 Module 3: Sky Image Retrieval & FITS-based Target Plotting
-Module 4: Robotic Arm Positioning Simulation for TA-MOONS")
+Module 4: Robotic Arm Positioning Simulation for TA-MOONS """)
 
 # --- Helper function for downloads ---
 @st.cache_data
