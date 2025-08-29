ed01301b36c73b8fe8a82d8ddbbc2051c6cddb67
KARTIK GHANSHYAM GOKHE <gokhekartik@gmail.com>
2025-08-29 15:22:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,7 @@ import io
 # --- App Configuration ---
 st.set_page_config(layout="wide", page_title="TA-MOONS Arm Simulator")
 st.title("🛰️ TA-MOONS Robotic Arm Positioning Simulator")
-st.write("""
-This project focuses on designing and simulating a collision-free arm positioning mechanism for the TA-MOONS front optics using methodologies inspired by the K-band Multi-object Spectrograph (KMOS) of the Very Large Telescope (VLT) and Multi-object Spectroscopic Mode (MOS) of the High-Resolution Optical Spectrograph (HROS) of the Thirty Meter Telescope (TMT). The system allows precise allocation of targets within a circular field of view based on celestial coordinates. A robust collision avoidance algorithm ensures safe operation, while Monte Carlo simulations validate the system’s reliability. This work demonstrates the integration of celestial mechanics, optical physics, and computational engineering to achieve a scalable solution for next-generation ground-based survey telescopic instruments.
-""")
+st.write(""" Pre Observation Preparation for Young Stellar Objects (YSO) Spectroscopic Survey Science Case """)
 
 # --- Helper function for downloads ---
 @st.cache_data
